122ec53c8ad1ff323f4123120886cd54de3e4c70
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-06-26 17:09:40 -0700

    I added a project to my site.

--- my-portfolio/src/components/ProjectCard.js
+++ my-portfolio/src/components/ProjectCard.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function ProjectCard({ title, description, imageUrl, projectUrl }) {
   return (
-    <div className="col-md-4">
+    <div className="col-lg-3 col-md-6 mb-4">
       <div className="card h-100 hover-shadow">
         <img src={imageUrl} className="card-img-top img-fluid" alt={title} />
-        <div className="card-body">
+        <div className="card-body d-flex flex-column">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href={projectUrl} className="btn btn-outline-primary custom-btn">View Project</a>
+          <a href={projectUrl} className="btn btn-outline-primary custom-btn mt-auto" aria-label={`View project: ${title}`}>
+            View Project
+          </a>
         </div>
       </div>
     </div>
--- my-portfolio/src/components/ProjectSection.js
+++ my-portfolio/src/components/ProjectSection.js
@@ -8,19 +8,26 @@ function ProjectSection() {
       <div className="row">
         {/* Project 1 */}
         <ProjectCard 
+          title="DevFlix" 
+          description="An innovative web platform for movie lovers, utilizing React for an interactive experience. Discover, and enjoy your favorite films effortlessly." 
+          imageUrl="./assets/movie.webp" 
+          projectUrl="https://devflixprime.netlify.app/" 
+        />
+        {/* Project 2 */}
+        <ProjectCard 
           title="Silvanus Grove" 
           description="A sleek web app for plant enthusiasts, featuring React, Firebase, and Stripe for a seamless shopping experience." 
           imageUrl="./assets/plant.jpg" 
           projectUrl="https://symphonious-sable-ca8f44.netlify.app/" 
         />
-        {/* Project 2 */}
+        {/* Project 3 */}
         <ProjectCard 
           title="Wordle Clone" 
           description="A compact web-based version of the famous word game, offering a 5-letter puzzle challenge with color-coded hints."
           imageUrl="./assets/wordle.jpg" 
           projectUrl="https://ultimamaximus.github.io/Wordle-Clone/" 
         />
-        {/* Project 3 */}
+        {/* Project 4 */}
         <ProjectCard 
           title="Muse Playlist Creator" 
           description="A creative, efficient React app for easy Spotify playlist creation, syncing playlists directly to your account using the Spotify API."
